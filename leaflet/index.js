5ffab085bd956e00db6e1d86bdbfbbec287c4095
janasluo <janas_luo@126.com>
2020-08-05 22:24:30 +0800

    test

--- leaflet/index.js
+++ leaflet/index.js
@@ -1,14 +1,20 @@
 
 var map = L.map('map').setView([
-  // 30.6506221395211790, 114.3123096125275300
-  // 28.737590, 115.878260
-  31.486242, 121.518368
+  30.6506221395211790, 114.3123096125275300 // 武汉
+  // 28.737590, 115.878260 // 南昌
+  // 31.486242, 121.518368 // 上海
+  // 39.89941, 116.37207 // 北京
+  // 38.87, 115.47  // 保定
+  // 30.9398270690073, 113.925285596662 // 孝感
 ], 12);
 var layers;
-var baseLayer = L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: 'Map data &copy; <a href=" ">OpenStreetMap</a > contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a >, Imagery © <a href="http://cloudmade.com">CloudMade</a >',
-  maxZoom: 18
+// var url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' // openstreetmap  wgs84
+
+var url = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' // mapbox wgs84
+
+// var url = 'http://mt2.google.cn/vt/lyrs=m&scale=2&hl=zh-CN&gl=cn&x={x}&y={y}&z={z}' // 谷歌中国gcj02
+var baseLayer = L.tileLayer(url, {
+  maxZoom: 18,
 }
 );
 baseLayer.addTo(map)
@@ -52,7 +58,6 @@ function draw() {
 
 function caculateCenter(geometry) {
   const type = geometry.type
-  console.log('geometry', geometry)
   switch (type) {
     case 'Point': return geometry.coordinates;
     case 'LineString': return geometry.coordinates[0];
@@ -64,6 +69,21 @@ function caculateCenter(geometry) {
 }
 function parseWkt(str) {
   var geometry = Terraformer.WKT.parse(str);
+  console.log('geometry', geometry)
+  // 手动调整误差
+  // if (geometry.type === 'Polygon') {
+  //   geometry.coordinates = geometry.coordinates.map(coordinate => coordinate.map(item => [item[0] - 0.0008, item[1] + 0.0004]))
+  // }
+  // if (geometry.type === 'MultiPolygon') {
+  //   geometry.coordinates = geometry.coordinates.map(coordinates => coordinates.map(coordinate => coordinate.map(item => [item[0] - 0.0008, item[1] + 0.0004])))
+  // }
+  // 坐标系转换
+  if (geometry.type === 'Point') {
+    const pArry = geometry.coordinates
+    const pObj = window.GeoUtil.transform({ lat: Number(pArry[1]), lng: Number(pArry[0]) }, "wgs84", "gcj02")
+    geometry.coordinates = [pObj.lng, pObj.lat]
+  }
+
   return [{
     "type": "Feature",
     "geometry": geometry,
